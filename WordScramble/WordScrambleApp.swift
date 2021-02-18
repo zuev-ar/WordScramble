1dc88ccc7ebfddc4f401d828deb624e181deae8f
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-02-18 20:46:18 +0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Arkasha Zuev on 18.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
